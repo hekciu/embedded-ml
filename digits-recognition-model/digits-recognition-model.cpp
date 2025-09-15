49988b59aa40af082ca8d98cbfdaea48973e6a50
Jakub Lasecki <jakublasecki321@gmail.com>
2025-09-15 22:05:23 +0200

    setup for basic opencv cmake project

---
+++ digits-recognition-model/digits-recognition-model.cpp
@@ -0,0 +1,20 @@
+﻿// digits-recognition-model.cpp: definiuje punkt wejścia dla aplikacji.
+//
+
+#include <iostream>
+#include <opencv2/opencv.hpp>
+
+using namespace std;
+
+int main()
+{
+	std::string image_path = "test_image.jpg";
+
+	cv::Mat img = cv::imread(image_path, cv::IMREAD_COLOR);
+
+	cv::imshow("Hello open CV", img);
+
+	cv::waitKey(0);
+
+	return 0;
+}
